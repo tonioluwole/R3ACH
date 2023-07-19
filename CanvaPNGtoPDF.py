323666128f70f0a30cd8ae1cf03ab0cb6983e35c
Toni Oluwole <59587344+tonioluwole@users.noreply.github.com>
2023-07-19 11:36:54 -0400

    added GUI for Program

--- CanvaPNGtoPDF.py
+++ CanvaPNGtoPDF.py
@@ -1,14 +1,22 @@
 #imported necessary libraries
+import os
 from zipfile import ZipFile
 from tkinter.filedialog import askopenfilename
 import img2pdf
+import PySimpleGUI as psg
 import glob
+import easyocr
 
-def converter():
-    # pass the name of the zip file to extract
-    filename = askopenfilename()
-    mypath = 'C:/Users/tonio/Downloads/DP/CanvaPNGs'
-    newpath = 'C:/Users/tonio/Downloads/DP/CanvaPDFs'
+"""
+filename = askopenfilename(title='Choose a file')
+"""
+
+def listreturn(filename):
+    foldername = filename[25:-4].replace(" ","")
+    print(foldername)
+    mypath = 'C:/Users/tonio/Downloads/DP/CanvaPNGs/'+foldername
+
+    newpath = 'C:/Users/tonio/Downloads/DP/CanvaPDFs/'+foldername
 
     # opening the zip file in read mode
     with ZipFile(filename, 'r') as zip:
@@ -16,19 +24,81 @@ def converter():
         zip.printdir()
 
         # extracting all the files
-        print('Extract in progress...')
         zip.extractall(path=mypath, members=None, pwd=None)
         print('All files are extracted!')
-        
-    #creating a list of all the extracted filenames
+
+    #creating a list of all the extracted filenames and 
     imglist = glob.glob(mypath+"\*")
-    print(imglist)
 
-    #naming the new pdf
-    print("Please type file name of new pdf")
-    name = input()
+    return imglist
 
+def reader(list):
+    #Reading text from the first image with oCR
+    print('Getting text from first image...')
+    reader = easyocr.Reader(['en'])
+    read = reader.readtext(list[0], detail = 0)
+    rname = ' '.join(read)
+    pdfname = rname[14:].replace(':','')
+
+    return pdfname
+
+def namer (inp,returnedlist):
     #creating the new pdf
-    with open(newpath+name+".pdf","wb") as f:
-        f.write(img2pdf.convert(imglist))
-        print('PDF has been created!')
+    with open(inp+".pdf","wb") as f:
+        f.write(img2pdf.convert(returnedlist)) #need to write to specific path
+
+
+column_layout = [
+    []
+]
+
+def new1():
+    return [
+        [psg.Text("All files are extracted!")],
+        [psg.Text("Getting text from first image...")]
+    ]
+
+def new3():
+    return [
+        [psg.Text("PDF has been created!")]
+    ]
+
+def new2():
+    return [
+        [psg.T("1st page of pdf is: "+life+"' \nWhat should file name be?\n")],
+        [psg.Input(enable_events=True, key='-INP-')],
+        [psg.Submit()]
+    ]
+
+layout = [
+   [psg.Text('Select a file',font=('monsterrat', 20), expand_x=True, justification='center')],
+   [psg.Input(enable_events=True, key='-IN-',font=('monsterrat', 12),expand_x=True), psg.FileBrowse()],
+   [psg.Button("Do the thing")],
+   [psg.Column(column_layout, key='-Column-')],
+   [psg.Button("Exit")]
+]
+
+
+window = psg.Window('FileChooser Demo', layout,
+size=(715,800))
+while True:
+   event, values = window.read()
+
+   if event == 'Do the thing':
+      filename = values["-IN-"]
+      imglist = listreturn(filename)
+      life = reader(imglist)
+      print(life)
+      window.extend_layout(window['-Column-'], new1())
+      window.extend_layout(window['-Column-'], new2())
+    
+   if event == 'Submit':
+       input = values['-INP-']
+       print(input)
+       namer(input,imglist)
+       window.extend_layout(window['-Column-'], new3())
+
+   if event == psg.WIN_CLOSED or event == 'Exit':
+      break
+window.close()
+
